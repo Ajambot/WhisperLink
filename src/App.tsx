f04c38036f7903a44cc567fa35f260221e40f3ba
Dev Kothari <dev.kothari15112002@gmail.com>
2024-03-20 12:39:58 -0400

    Added a basic Home page with 2 buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import { useEffect, useState } from 'react'
+import React from 'react';
+import HomePage from 'C:/Users/devko/WhisperLink-1/src/components/HomePage.tsx';
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { connectFirestoreEmulator, collection, query, onSnapshot, getFirestore} from 'firebase/firestore';
@@ -54,6 +56,16 @@ function App() {
       })}
     </>
   )
+
+  return (
+    <div className="App">
+        <HomePage
+            onJoin={() => console.log('Join button clicked')}
+            onCreate={() => console.log('Create button clicked')}
+        />
+    </div>
+);
 }
+
 
 export default App
---
+++ src/components/HomePage.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+interface HomePageProps {
+    onJoin: () => void;
+    onCreate: () => void;
+}
+
+const HomePage: React.FC<HomePageProps> = ({ onJoin, onCreate }) => {
+    return (
+        <div className="home">
+            <h1>Welcome to WhisperLink</h1>
+            <div className="buttons">
+                <button onClick={onJoin}>Join a Chatroom</button>
+                <button onClick={onCreate}>Create a Chatroom</button>
+            </div>
+        </div>
+    );
+};
+
+export default HomePage;
