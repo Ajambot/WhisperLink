baf79d5d53b0ea9e49b3fca2dba3dbb7792f85b1
Dev K <99576817+dkothari15@users.noreply.github.com>
2024-04-08 19:24:36 -0400

    image

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -9,12 +9,16 @@ interface HomePageProps {
 
 const HomePage: React.FC<HomePageProps> = ({ onJoin, onCreate }) => {
   return (
+    <div>
     <div className={styles.titleContainer}>
+    <div className={styles.imageContainer}>
       <div className={styles.message}> WhisperLink</div>
       <div className={styles.buttonContainer}>
         <button className={textStyles.textContainer} onClick={onJoin}>Join Chatroom</button>
         <button className={textStyles.textContainer} onClick={onCreate}>Create Chatroom</button>
       </div>
+    </div>
+    </div>
     </div>
   );
 };
